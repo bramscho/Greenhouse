e5a5f5247f4cfc9f227ab4356b6a93aaf5d12b72
Bram Schork <Bram@Bram-Air.home>
2017-08-18 17:58:27 -0400

    692

--- MERGE_FINAL_TEST_1/MERGE_FINAL_TEST_1.ino
+++ MERGE_FINAL_TEST_1/MERGE_FINAL_TEST_1.ino
@@ -1,5 +1,5 @@
 /*
-  How to connect temop.humidity sensor:
+  How to connect temp.humidity sensor:
   Connect pin 1 (on the left) of the sensor to +5V
   NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
   to 3.3V instead of 5V!
@@ -7,45 +7,33 @@
   Connect pin 4 (on the right) of the sensor to GROUND
   Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
   Initialize DHT sensor.
-  Note that older versions of this library took an optional third parameter to
-  tweak the timings for faster processors.  This parameter is no longer needed
-  as the current DHT reading algorithm adjusts itself to work on faster procs.
+
+light - timer
+soil heater- timer
+
+soil moisture/watering - waterbot
+
+PC fan - 24/7
+
+soil temp/soil moisture/ph level - electronic test probe thingie
+
+air temp/humidity - sensor and little weather station
+
+ALL READING WILL BE READ BY SERIAL MONITOR
 */
 
-//TEMP/HUMIDITY Libraries
-#include "DHT.h"
+#include "DHT.h" // temp/humidity sensor library
 
-//TEMP/HUMIDITY Defines
-#define DHTPIN 1 //TEMP/HUMIDTY DIGITAL PIN
-#define DHTTYPE DHT11
+#define DHTPIN 1 //what digit pin the temp/humidity sensor will be plugged into
+#define DHTTYPE DHT11 // define what type of sensor it is
 
-//TEMP/HUMIDITY Other Stuff
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // other stuff for the temp/humidity sensor
 
-//LCD/SOIL Libraries
-#include <Wire.h>
-#include "LCD.h"
-#include "LiquidCrystal_I2C.h"
+int n = 1; //soil moisture other stuff
 
-//LCD/SOIL Defines
-#define I2C_ADDR 0x27
-#define BACKLIGHT_PIN     3
-#define En_pin  2
-#define Rw_pin  1
-#define Rs_pin  0
-#define D4_pin  4
-#define D5_pin  5
-#define D6_pin  6
-#define D7_pin  7
-
-//LCD/SOIL Other Stuff
-int n = 1;
-LiquidCrystal_I2C  lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, D7_pin);
-
-//LCD/SOIL Int's
 int soilSensorPin = A0; //SOIL SENSOR PIN
-int soilSensorValue = 0;  // variable to store the value coming from the sensor
-int soilSensorPercentage = 0;
+int soilSensorValue = 0;  ////variable to hold the incoming soil moisture value at
+int soilSensorPercentage = 0; //variable to hold the incoming soil moisture value at
 int solonoidPin = 2; //SOLONOID DIGITAL PIN
 
 void setup() {
@@ -53,12 +41,6 @@ void setup() {
   Serial.begin(9600); //start serial monitor at 9600 baud(bits per second)
 
   dht.begin(); //start the temp/humidity sensor
-
-  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE); //set the LCD's backlight as positive
-  lcd.setBacklight(HIGH); // turn on the LCD's backlight
-
-  lcd.begin(16, 2);//start the LCD and set up the LCD's number of columns and rows
-
 }
 
 void loop() {
@@ -66,54 +48,41 @@ void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature(); // Read temperature as Celsius (the default)
   float f = dht.readTemperature(true); // Read temperature as Fahrenheit (isFahrenheit = true)
-
-  // Check if any reads failed and exit early (to try again).
+ 
+   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  Serial.println("Failed to read from DHT sensor!");
+  return; 
   }
-
-  float hif = dht.computeHeatIndex(f, h); // Compute heat index in Fahrenheit (the default)
-  float hic = dht.computeHeatIndex(t, h, false); // Compute heat index in Celsius (isFahreheit = false)
 
   Serial.print("Humidity: ");
   Serial.print(h);
-  Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.print(" *C ");
   Serial.print(f);
   Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
-
+ 
   soilSensorValue = analogRead(soilSensorPin); // read the value from the soil sensor:
   soilSensorPercentage = ((1023 - soilSensorValue) / 10.23); //convert the incoming analog reading to a percentage
 
   Serial.print("soil sensor = " );
-  Serial.println(soilSensorPercentage);
-  lcd.setCursor(0, 1);
-  lcd.print("  Moisture: ");
-  lcd.print(soilSensorPercentage);
-  lcd.print("%        ");
+  Serial.print(soilSensorPercentage);
+  Serial.print("  Moisture: ");
+  Serial.print(soilSensorPercentage);
+  Serial.print("%        ");
 
   if (soilSensorPercentage < 30) {
-    lcd.setCursor(4, 0);
-    Serial.println("Water On");
+    Serial.print("Water On");
     digitalWrite(2, HIGH);//if water level is below desired percentage, send power to solenoid
-    lcd.print("Water On    ");
+    Serial.print("Water On    ");
     digitalWrite(2, LOW);
   }
   else
   {
-    lcd.clear();
-    lcd.setCursor(4, 0);
     Serial.println("Water Off");
     digitalWrite(2, LOW);//if water level is at desired percentage, shut off power to the solenoid
-    lcd.print("Water Off");
+    Serial.print("Water Off");
     digitalWrite(2, HIGH);
   }
   delay(500);
